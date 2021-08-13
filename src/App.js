41a5b6f50250e316be38d378b047c672caee5a20
priyanka <priyankajadhav31@gmail.com>
2021-08-13 10:17:31 +0530

    My first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Welcome to my First React Page
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
